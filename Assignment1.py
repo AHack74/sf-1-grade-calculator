99c1431e81c2d7d15423edb969a99fc1dfd8c6df
Aidan Hacking <aidanhacking@Aidans-MacBook-Air.local>
2024-09-25 15:46:45 -0400

    Final product

--- Assignment1.py
+++ Assignment1.py
@@ -9,18 +9,19 @@ midterm_2 = input("Enter grade for Midterm 2: ")
 final_exam = input("Enter grade for Final Exam: ")
 midterm_final_prep = input("Enter grade for Midterm and Final Preparation: ")
 
-if (num_of_labs >= 6):
+if int(num_of_labs) >= 6:
 	labs = 100 * 0.2
-else (num_of_labs < 6): 
-	labs = (num_of_labs/6) * 0.2
-if (num_of_quiz >= 6):
+else: 
+	labs = int(num_of_labs)/6 * 100 * 0.2
+if int(num_of_quiz) >= 6:
 	quiz = 100 * 0.15
-else (num_of_quiz < 6):
-	quiz = (num_of_quiz/6) * 0.15
-assignments = (assignment_1 + assignment_2 + assignment_3 + assignment_4) / 4 * 0.16
-midterms = (midterm_1 + midterm_2) / 2 * 0.25 
-final = final_exam * 0.18
-mfp = midterm_final_prep * 0.06
+else:
+	quiz = int(num_of_quiz)/6 * 100 * 0.15
+
+assignments = (int(assignment_1) +int(assignment_2) + int(assignment_3) + int(assignment_4)) / 4 * 0.16
+midterms = (int(midterm_1) + int(midterm_2)) / 2 * 0.25 
+final = int(final_exam) * 0.18
+mfp = int(midterm_final_prep) * 0.06
 
 grade = labs + quiz + assignments + midterms + final + mfp 
-print("Your grade is: "grade) 
+print("Your grade is: " + str(round(grade, 0))) 
